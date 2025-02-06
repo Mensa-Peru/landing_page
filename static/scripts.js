0787ba1a775139e67ed904d6b15783ca0bd604a1
Ruben Ignacio Guzman Zamora <ruben.ignaciogz@gmail.com>
2025-02-06 09:09:59 -0500

    Refactor slideshow implementation and update HTML structure

--- static/scripts.js
+++ static/scripts.js
@@ -1,72 +1,78 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
+class SlideShow {
+  timer = null;
+  currentIndex = 0;
+  currentSlide = null;
 
-// Slideshow Functionality
-let slideIndices = [1, 1];
-let slideId = ["slides1", "slides2"];
-let timers = [];
+  constructor(slideSelectorOrElem) {
+    if (typeof slideSelectorOrElem === 'string') {
+      this.slideElem = document.querySelector(slideSelectorOrElem);
+    } else {
+      this.slideElem = slideSelectorOrElem;
+    }
 
-// Initialize slideshows
-for (let i = 0; i < slideId.length; i++) {
-  showSlides(slideIndices[i], i);
-  setTimer(i);
-}
+    this.slides = this.slideElem.querySelectorAll('.slides > *');
+    this.totalSlides = this.slides.length;
 
-// Next/previous controls
-function plusSlides(n, no) {
-  clearTimeout(timers[no]);
-  slideIndices[no] += n;
-  showSlides(slideIndices[no], no);
-  setTimer(no);
-}
-
-// Show slides function
-function showSlides(n, no) {
-  let slidesContainer = document.getElementById(slideId[no]);
-  let slides = document.querySelectorAll(`#${slideId[no]} img`);
-  let totalSlides = slides.length;
-
-  if (n > totalSlides) { slideIndices[no] = 1; }
-  if (n < 1) { slideIndices[no] = totalSlides; }
-
-  // Before starting the transition, make all images visible
-  slides.forEach((slide) => {
-    //slide.style.visibility = "visible";
-    slide.classList.add('active');
-  });
-
-  // Apply the transform to slide the images
-  let translateX = -(slideIndices[no] - 1) * 100;
-  slidesContainer.style.transform = `translateX(${translateX}%)`;
-
-  // Trigger fade out
-  slides.forEach((slide, index) => {
-    if (index + 1 !== slideIndices[no])
-      slide.classList.remove('active');
-    //slide.style.visibility = (index + 1 === slideIndices[no]) ? "visible" : "hidden";
-  });
-
-  // Event listener for transition end
-  function transitionEndHandler() {
-    // After the transition, hide all slides except the current one
-    // Remove this event listener
-    slidesContainer.removeEventListener('transitionend', transitionEndHandler);
+    this.addControlListener();
+    this.loop();
   }
 
-  slidesContainer.addEventListener('transitionend', transitionEndHandler);
-}
+  addControlListener() {
+    const sliderNav = this.slideElem.querySelector('.navigation');
 
-// Automatic slideshow with 5-second interval
-function setTimer(no) {
-  timers[no] = setTimeout(function () {
-    plusSlides(1, no);
-  }, 5000);
+    sliderNav.addEventListener('click', (ev) => {
+      const navButton = ev.target.closest('.next, .prev');
+
+      if (!navButton) return;
+
+      if (navButton.classList.contains('next')) {
+        this.nextSlide();
+      } else {
+        this.prevSlide();
+      }
+    });
+  }
+
+  nextSlide() {
+    clearTimeout(this.timer);
+
+    this.currentIndex++;
+
+    if (this.currentIndex >= this.totalSlides) {
+      this.currentIndex = 0;
+    }
+
+    this.showSlide();
+    this.loop();
+  }
+
+  prevSlide() {
+    clearTimeout(this.timer);
+
+    if (!this.currentIndex) {
+      this.currentIndex = this.totalSlides;
+    }
+
+    this.currentIndex--;
+
+    this.showSlide();
+    this.loop();
+  }
+
+  showSlide() {
+    if (this.currentSlide) {
+      this.currentSlide.classList.remove('active');
+    }
+
+    this.currentSlide = this.slides[this.currentIndex];
+
+    this.currentSlide.classList.add('active');
+  }
+
+  // Automatic slideshow with 5-second interval
+  loop() {
+    this.timer = setTimeout(() => { this.nextSlide() }, 5000);
+  }
 }
 
 function loadPreloadedStyles() {
@@ -104,4 +110,7 @@ window.addEventListener('load', () => {
       document.body.dataset.navbarExpanded = false;
     }
   });
+
+  // Initialize slideshow
+  document.querySelectorAll('.slider').forEach(elem => new SlideShow(elem));
 });
